43961046f4bfa1dd0d29da9a894b57efbedee98d
Daniel Ekedahl <ghostnr1@gmail.com>
2020-03-20 14:24:05 +0000

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ USER root
 WORKDIR /app
 COPY . /app
 RUN npm install
+RUN npm uglify
 EXPOSE 8080
 CMD [ "node", "server.js" ]
--- resources/script/docker.js
+++ resources/script/docker.js
@@ -6,46 +6,46 @@ $(function() {
   startWs();
 
   var shellprompt = '$ ';
-  
+
   window.addEventListener("resize", function () {
     document.getElementById('terminal-continer').style.height = "100%";
     globalValues.xtermFitAddon.fit()    
   });
-  
+
   $('#terminal-tab').on('shown.bs.tab', function () {
     if (!globalValues.xterm) {
-    let termContainer = document.getElementById('terminal-continer');
-    
-    globalValues.xterm = new Terminal({
-      cursorBlink: true
-    });
-    globalValues.xterm.open(termContainer);
-    globalValues.xtermFitAddon = new FitAddon();
- 
-    globalValues.xterm.loadAddon(globalValues.xtermFitAddon);
+      let termContainer = document.getElementById('terminal-continer');
 
-    globalValues.xterm.write("~$ ");
-    
-    var input = "";
-    globalValues.xterm.onData(function(data) {
-      const code = data.charCodeAt(0);
-      if (code == 13) { // CR
-        ws.getSubscription('docker:terminal').emit('dockerCommand', {
-          message: input
-	})
-	input = ''
-      } else if (code < 32 || code == 127) { // Control
-        return;
-      } else { // Visible
-        globalValues.xterm.write(data);
-        input += data;
-      }
-    })
-		document.getElementById('terminal-continer').style.height = "100%";
+      globalValues.xterm = new Terminal({
+        cursorBlink: true
+      });
+      globalValues.xterm.open(termContainer);
+      globalValues.xtermFitAddon = new FitAddon();
+
+      globalValues.xterm.loadAddon(globalValues.xtermFitAddon);
+
+      globalValues.xterm.write("~$ ");
+
+      var input = "";
+      globalValues.xterm.onData(function(data) {
+        const code = data.charCodeAt(0);
+        if (code == 13) { // CR
+          ws.getSubscription('docker:terminal').emit('dockerCommand', {
+            message: input
+          })
+          input = ''
+        } else if (code < 32 || code == 127) { // Control
+          return;
+        } else { // Visible
+          globalValues.xterm.write(data);
+          input += data;
+        }
+      })
+      document.getElementById('terminal-continer').style.height = "100%";
       globalValues.xtermFitAddon.fit()
     }
   })
-  
+
 })
 
 function startWs() {
