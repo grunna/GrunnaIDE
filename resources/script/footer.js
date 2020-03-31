f49bbf76eb81a5fa40fde8c58b3025e2e91e09c9
Daniel Ekedahl <ghostnr1@gmail.com>
2020-03-31 20:27:29 +0000

    #21 missed new js footer file

---
+++ resources/script/footer.js
@@ -0,0 +1,32 @@
+"use strict";
+
+$(function () {
+  
+  let sortedModeInfo = CodeMirror.modeInfo.slice().sort((a,b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+
+    let comparison = 0;
+    if (nameA > nameB) {
+      comparison = 1;
+    } else if (nameA < nameB) {
+      comparison = -1;
+    }
+    return comparison;
+  })
+  sortedModeInfo.forEach(mode => {
+    let modeLink = document.createElement('a')
+    modeLink.id = mode.name
+    modeLink.innerHTML = mode.name
+    modeLink.classList.add("dropdown-item")
+    $('#dropdownModeMenu').append(modeLink)
+  })
+  
+  $('#dropdownModeMenu a').on('click', (event) => {
+    const modeName = $(event.currentTarget).attr('id')
+    let selectedMode = CodeMirror.findModeByName(modeName)
+    setCurrentMode(selectedMode.mode, selectedMode.mime, modeName)
+  })
+  
+
+});
