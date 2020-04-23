65a2891ee469b012575f0d3fb2c9383dcc1cbdbe
Daniel Ekedahl <ghostnr1@gmail.com>
2020-04-23 20:27:54 +0000

    #51 Bug when empty statistics

--- app/Controllers/Http/DashboardController.js
+++ app/Controllers/Http/DashboardController.js
@@ -7,14 +7,6 @@ const Shared = use('./Shared')
 const shared = new Shared()
 
 class DashboardController {
-/*
-   $('#currentProjectsFooter').text(data.projects + ' / ' + data.maxProjects + ' Project created')
-      $('#directorySize').text('All your projects take ' + (data.directorySize / 1024 / 1024).toFixed(2) + ' MB')
-      $('#saveTimes').text('You have saved ' + (data.statistics.saveTimes ? data.statistics.saveTimes : 0) + ' times')
-      $('#reloadFileTree').text('You have reloaded the tree ' + (data.statistics.reloadFileTree ? data.statistics.reloadFileTree : 0) + ' times')
-      $('#filesDownloaded').text((data.statistics.filesDownloaded ? data.statistics.filesDownloaded : 0) + ' files have been downloaded')
-      $('#fileCreated').text('You have created ' + (data.statistics.fileCreated: 0,  ? data.statistics.fileCreated : 0) + ' files')
-      $('#deleteFileDirectory').text('You have deleted ' + (data.statistics.deleteFileDirectory ? data.statistics.deleteFileDirectory :*/
   
   async summery({response, request, auth}) {
     let summery = { projects: 0, maxProjects: 0, directorySize: 0, statistics: {saveTimes: 0, reloadFileTree: 0, filesDownloaded: 0, fileCreated: 0, deleteFileDirectory: 0}}
